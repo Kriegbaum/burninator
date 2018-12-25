ea87fbec36bd10edbe3547185326e2f04c3fd280
root <spid3rbox@gmail.com>
2018-12-24 21:16:24 -0500

    I DID A WHOLE Lot of thifngs tio the orpogram in HTTHIS COMMIT HERE

--- thermostat.py
+++ thermostat.py
@@ -4,6 +4,35 @@ import adafruit_mcp9808
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 import threading
 import time
+import os
+import datetime
+import gpiozero
+from signal import pause
+
+#LOOK INTO PID ALGORITHMS
+
+#################GRAB LOCAL IP ADDRESS##########################################
+ipSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    ipSock.connect(('10.255.255.255', 1))
+    localIP = ipSock.getsockname()[0]
+except:
+    localIP = '127.0.0.1'
+ipSock.close()
+socket.setdefaulttimeout(1)
+
+##################RELAY CONTROL OBJECT##########################################
+relayPin = 'PUT PIN HERE'
+relay = gpiozero.DigitalOutputDevice(relayPin)
+
+##########################BUTTONS###############################################
+up = gpiozero.Button(board.D3, bounce_time=0.2)
+down = gpiozero.Button(board.D2, bounce_time=0.2)
+left = gpiozero.Button(board.D10, bounce_time=0.2)
+right = gpiozero.Button(board.D9, bounce_time=0.2)
+select = gpiozero.Button(board.D8, bounce_time=0.2)
+reset = gpiozero.Button(board.D7, bounce_time=0.2)
+
 
 ##############PHYSICAL DEVICES ATTACHED TO BOARD################################
 lcd_columns = 16
@@ -16,30 +45,76 @@ lcd.color = [0,0,100]
 lcd.message = 'Initializing\nBurninator...'
 
 
-##################CONTROL OBJECTS###############################################
-class Burninator:
-    def __init__(self, home, away, sleep):
-        #Poll this value to see whether or not the furnace should be engaged
-        self.burn = False
-        #Poll this to check home/away state
-        self.state = 'away'
-        #Various temperature setpoints and readings
-        self.currentTemp = False
-        self.homeTemp = home
-        self.awayTemp = away
-        self.sleepTemp = sleep
 
-########################DATA GATHERING OBJECTS##################################
-class DataCollector:
-    def __init__(self):
-        #Time in seconds that the furnace has been running today
-        self.burnTime = 0
-        #Use these two to calculate average daily set point
-        self.avgSetPoint = 0
-        self.avgSetPointIterator = 0
+######################DATA COLLECTOR CONTROL OBJECTS############################
+burnStart = 0
+#Time in seconds that the furnace has been running today
+burnTime = 0
+#Use these two to calculate average daily set point
+avgSetPoint = 0
+avgSetPointIterator = 0
+logFile = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'burnLog.csv'), 'a+')
 
+def writeData(self):
+    data = (datetime.datetime.now(), self.avgSetPoint, self.burnTime)
+    burnTime = 0
+    avgSetPoint = 0
+    avgSetPointIterator = 0
+    logFile.write(data)
+
+########################THERMOSTAT CONTROL OBJECTS##############################
+#Poll this value to see whether or not the furnace should be engaged
+burn = False
+remoteSensors = False
+#Poll this to check the home/away state
+#Valid values, 'home', 'away', 'sleep'
+state = 'away'
+deadband = 3
+currentTemp = mcp.temperature * 9 / 5 + 32
+
+homeTemp = 69
+awayTemp = 59
+sleepTemp = 62
+burnStart = False
+setPoint = awayTemp
+
+def burn():
+    if burn:
+        return True
+    else:
+        burn = True
+        burnStart = datetime.datetime.now()
+        relay.on()
+def halt():
+    if burn:
+        burn = False
+        burnTime += datetime.datetime.now() - burnStart
+        relay.off()
+    else:
+        return False
+def getTempLocal():
+    return mcp.temperture * 9 / 5 + 32
+def getTempGlobal():
+    pass
+def getTemp():
+    if remoteSensors:
+        return getTempGlobal()
+    else:
+        return getTempLocal()
 
 ##########################INTERFACE FUNCTIONS###################################
+def plusMax(value, max):
+    value += 1
+    if value > max:
+        value = max
+    return value
+
+def minusMin(value, min):
+    value -= 1
+    if value < min:
+        value = min
+    return value
+
 class Interface:
     def __init__(self, timeout, ):
         self.page = 0
@@ -65,6 +140,7 @@ class Interface:
             self.page = 0
         lcd.clear()
         self.display()
+        self.bump()
 
     def prevPage(self):
         self.page -= 1
@@ -72,29 +148,81 @@ class Interface:
             self.page = self.pages
         lcd.clear()
         self.display()
+        self.bump()
 
+    def increaseValue(self):
+        if page == 0:
+            setPoint = plusMax(setPoint, maxTemp)
+            self.display()
+            self.bump()
+        elif page == 1:
+            homeTemp = plusMax(homeTemp, maxTemp)
+            self.display()
+            self.bump()
+        elif page == 2:
+            awayTemp = plusMax(awayTemp, maxTemp)
+            self.display()
+            self.bump()
+        elif page == 3:
+            sleepTemp = pluxMax(sleepTemp, maxTemp)
+            self.display()
+            self.bump()
+    def decreaseValue(self):
+        if page == 0:
+            setPoint = minusMin(setPoint, maxTemp)
+            self.display()
+            self.bump()
+        elif page == 1:
+            homeTemp = minusMin(homeTemp, maxTemp)
+            self.display()
+            self.bump()
+        elif page == 2:
+            awayTemp = minusMin(awayTemp, maxTemp)
+            self.display()
+            self.bump()
+        elif page == 3:
+            sleepTemp = minusMin(sleepTemp, maxTemp)
+            self.display()
+            self.bump()
     def display():
         if page == 0:
             lcd.message = "%s, %f F\nCurrent Set: %fF"
         elif page == 1:
+            lcd.message = "%s, %f F\nHome Temp: %fF"
         elif page == 2:
+            lcd.message = "%s, %f F\nAway Temp: %fF"
         elif page == 3:
-
+            lcd.message = "%s, %f F\nSleep Temp: %fF"
 
 
 
 ##########################MAIN THREAD FUNCTIONS#################################
+interface = Interface()
 
 def interfaceThread():
     '''polls buttons for change in state, updates display output'''
-    pass
+
+    while True:
+        up.when_pressed = interface.increaseValue
+        down.when_pressed = interface.decreaseValue
+        left.when_pressed = interface.prevPage
+        right.when_pressed = interface.nextPage
+        select.when_pressed = idontfuckinknow
+        up.when_held = interface.increaseValue
+        down.when_held = interface.decreaseValue
+        pause()
 
 def burninator():
     '''Main action thread for thermostat, regulates temperature and logs data'''
-    pass
+    while True:
+        currentTemp = getTemp()
+        if currentTemp < setPoint - deadband:
+            burn()
+        elif currentTemp > setPoint + deadband:
+            halt()
+        else:
+            pass
 
-def stateGetServer():
+def stateServer():
     '''listens for commands from other devices regarding set-point and home/away state'''
     pass
-
-print(mcp.temperature * 9 / 5 + 32)
